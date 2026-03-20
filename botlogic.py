357641e2cf5ca1635afd51d5656188ffd2faae34
Ноутбук Ezhik <ezhik@gmail.com>
2026-03-20 19:08:00 +0300

    фзш

--- botlogic.py
+++ botlogic.py
@@ -2,7 +2,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
   
   
 def payment_keyboard(stars_amount):  
-    builder = InlineKeyboardBuilder()  
-    builder.button(text=f"Оплатить {stars_amount} звёзд", pay=True, style='success', icon_custom_emoji_id=5422367241645611298)  
+     builder = InlineKeyboardBuilder()  
+     builder.button(text=f"Оплатить {stars_amount} звёзд", pay=True, style='success', icon_custom_emoji_id='5422367241645611298')  
   
-    return builder.as_markup()
+     return builder.as_markup()
