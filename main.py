378652744a50185c426b4bbf87d658936d7f295e
Ноутбук Ezhik <ezhik@gmail.com>
2026-04-12 14:03:35 +0300

    api

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import re
 import sys
 import time
 from aiogram.types import LabeledPrice, Message, PreCheckoutQuery
+from aiogram.types.copy_text_button import CopyTextButton
 from botlogic import payment_keyboard 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
@@ -905,8 +906,7 @@ async def subscription_callback(callback: types.CallbackQuery):
         if is_enabled:
             subscription_keyboard = InlineKeyboardMarkup(
                 inline_keyboard=[
-                    [InlineKeyboardButton(text="Использовать (v2RayTun)", url=f"v2raytun://import/https://www.ezhqpy.ru/uMp0MVDJNm/{sub_id}", callback_data=f"use_sub_{sub_id}", style="primary", icon_custom_emoji_id='5271604874419647061')],
-                    [InlineKeyboardButton(text="Использовать (Hupp)", url=f"hupp://add/https://www.ezhqpy.ru/uMp0MVDJNm/{sub_id}", callback_data=f"use_sub_{sub_id}", style="primary", icon_custom_emoji_id='5271604874419647061')],
+                    [CopyTextButton(text="Использовать (Hupp)",  style="primary", icon_custom_emoji_id='5271604874419647061')],
                     [InlineKeyboardButton(text="Продлить подписку", callback_data="renew_subscription", style="primary", icon_custom_emoji_id='5231012545799666522')],
                     [instruction_btn],
                     [InlineKeyboardButton(text="Назад", callback_data="main_menu", style="danger")]
